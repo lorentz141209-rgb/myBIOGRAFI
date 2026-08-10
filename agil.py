83e8552e7bd72ff8ac9113ef1b8a709d8c6c201a
lorentz141209-rgb <lorentz141209@gmail.com>
2026-08-11 00:09:21 +0800

    Update agil.py

--- agil.py
+++ agil.py
@@ -4,19 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Data biografi yang dikirimkan ke HTML
-    profile_data = {
-        "nama": "GILLZZ",
-        "sekolah": "MAN 2 Kota Palu",
-        "deskripsi": "Siswa yang aktif, berprestasi, dan memiliki ketertarikan tinggi pada dunia ekonomi, teknologi, serta aktivitas organisasi sekolah.",
-        "prestasi": [
-            "Juara / Penghargaan Perlombaan Siswa (PMR & Organisasi)",
-            "Aktif dalam kepengurusan dan kegiatan ekstrakurikuler sekolah",
-            "Mewakili MAN 2 Kota Palu dalam ajang kompetisi antar pelajar"
-        ],
-        "images": ["foto1.jpeg", "foto2.jpg", "foto3.jpg"]
-    }
-    return render_template('index.html', data=profile_data)
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
